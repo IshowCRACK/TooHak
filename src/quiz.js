7d07c8be2faf44ed4e46ef027d397bf5f50fecf4
Kelly Su <z5418860@ad.unsw.edu.au>
2023-06-07 02:29:49 +1000

    Adding adminQuizCreate

---
+++ src/quiz.js
@@ -0,0 +1,8 @@
+// implementation for the function adminQuizCreate given
+// Parameters and Return Object
+
+function adminQuizCreate(authUserId, name, description) {
+        return {
+                quizId: 2, 
+        }
+}
