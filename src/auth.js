46f30220aec121941d3fc0303085123fbbc57721
IJLAL <z5310722@ad.unsw.edu.au>
2023-06-06 19:11:41 +1000

    completed the stub code for authRegister

---
+++ src/auth.js
@@ -0,0 +1,10 @@
+// implementation for the function adminAuthRegister given
+// Parameters and Return Object
+
+function adminAuthRegister(email, password, nameFirst, nameLast) {
+
+    return {
+        authUserId: 1,
+    }
+}
+  
