7e95940bf1fb5a655d666f5daff4ba589ed88e69
Titus Charoensri <z5419062@ad.unsw.edu.au>
2023-06-07 12:50:15 +1000

    added adminQuizList stub funciton

---
+++ src/quiz.js
@@ -0,0 +1,11 @@
+function adminQuizList(authUserId) {
+
+    return {
+        quizzes: [    
+            {      
+                quizId: 1,      
+                name: 'My Quiz',    
+            }  
+        ]
+    }
+}
