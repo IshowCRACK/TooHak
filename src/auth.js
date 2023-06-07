a89c3abc765b5189d2a68593a3f14853dcfac43f
IJLAL <z5310722@ad.unsw.edu.au>
2023-06-07 13:31:51 +1000

    finished authregister

--- src/auth.js
+++ src/auth.js
@@ -1,5 +1,5 @@
 // implementation for the function adminAuthRegister given
-// Parameters and Return Object
+// Parameters and Return object
 
 function adminAuthRegister(email, password, nameFirst, nameLast) {
 
