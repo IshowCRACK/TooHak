7d77ed5789d3e13ff958edb98c4cc805bb36a0b4
Titus Charoensri <z5419062@ad.unsw.edu.au>
2023-06-14 15:37:11 +1000

    Implimented clear function in other.js

--- src/other.js
+++ src/other.js
@@ -1,5 +1,12 @@
 // Reset the state of the application back to the start
 // takes no premeter and returns empty object
+import { getData, setData } from 'dataStore.js';
 function clear() {
+    const data = {
+    User = User[{}],
+    Quizes = Quizes[{}],
+    };
+    setData(data)
     return {};
 }
+module.export = clear;
