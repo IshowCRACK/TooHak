4523bf4cd2e278cd3366a3606fbc6faf96a1161f
IJLAL <z5310722@ad.unsw.edu.au>
2023-06-16 22:59:07 +1000

    started writing test

---
+++ src/auth.test.js
@@ -0,0 +1,52 @@
+import { adminAuthLogin, adminAuthRegister, adminUserDetails } from './auth.js';
+//import { clear } from './other.js'
+
+// test for adminAuthRegister
+test ('check successful login', () =>{
+    //clear();
+    let userId =
+
+
+});
+
+test ('check last name is valid', () =>{
+    //clear();
+
+
+
+});
+
+test ('check first name is valid', () =>{
+    //clear();
+
+
+
+});
+
+test ('check email is not duplicate', () =>{
+    //clear();
+
+
+
+});
+
+test ('check email is valid', () =>{
+    //clear();
+
+
+
+});
+
+test ('check password does not contain a letter and number', () =>{
+    //clear();
+
+
+
+});
+
+test ('check password is too short', () =>{
+    //clear();
+
+
+
+});
