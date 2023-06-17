6bb55c014916d3e18628faaf3a100a4221efcfab
IJLAL <z5310722@ad.unsw.edu.au>
2023-06-17 14:59:36 +1000

    changed a how we r storing and fixed style

--- src/dataStore.js
+++ src/dataStore.js
@@ -1,19 +1,15 @@
 // YOU SHOULD MODIFY THIS OBJECT BELOW
 let data = {
   users: [
-          {
+    {
 
-
-        }
+		},
   ],
   quizes: [
-            {
+		{
 
-              
-          }
-
-
-  ],  
+		},
+	],  
 };
 
 // YOU SHOULDNT NEED TO MODIFY THE FUNCTIONS BELOW IN ITERATION 1
