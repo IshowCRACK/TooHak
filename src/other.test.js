d9ff18b766d407eea36bcc9f003cf7195592aa06
IJLAL <z5310722@ad.unsw.edu.au>
2023-06-17 14:49:19 +1000

    fixed the test and style

--- src/other.test.js
+++ src/other.test.js
@@ -1,14 +1,15 @@
 //Clear Function Tests
-/*
-import { clear } from 'other.js';
-import { getData } from 'dataStore.js';
+
+import { clear } from './other.js';
+import { getData } from './dataStore.js';
+import { adminAuthRegister } from './auth.js'
 
 test('Test successful clear', () => {
-    let person1_id = adminAuthRegister('hayden.smith@unsw.edu.au','123ABCabc!@#$');
-    let quiz1_id= adminQuizCreate(person1_id,'how to drop comp1531', 'A comprehensive quiz to test students knowledge on how to quickly drop comp1531');
+    let person1_id = adminAuthRegister('good.l@gl.com','Password123','Joh nny-Bone',"Jo'nes");
+    //let quiz1_id= adminQuizCreate(person1_id,'how to drop comp1531', 'A comprehensive quiz to test students knowledge on how to quickly drop comp1531');
     
     clear();
-    expect(getData()).toBe(
+    expect(getData()).toStrictEqual(
         {
             users: [
                 {
@@ -25,4 +26,4 @@ test('Test successful clear', () => {
         } 
     );
   });
-  */
+  
