1cd387243a034b988b4a88da84bdab68ce004bbe
IJLAL <z5310722@ad.unsw.edu.au>
2023-06-17 17:58:55 +1000

    fixed the testing and is now working for authregister

--- src/other.test.js
+++ src/other.test.js
@@ -5,9 +5,7 @@ import { getData } from './dataStore.js';
 import { adminAuthRegister } from './auth.js'
 
 test('Test successful clear', () => {
-	let person1_id = adminAuthRegister('good.l@gl.com','Password123','Joh nny-Bone',"Jo'nes");
-	//let quiz1_id= adminQuizCreate(person1_id,'how to drop comp1531', 'A comprehensive quiz to test students knowledge on how to quickly drop comp1531');
-	
+	let person1_id = adminAuthRegister('good.l@gl.com','Password123','Joh nny-Bone',"Jo'nes");	
 	clear();
 	expect(getData()).toStrictEqual({
 		users: [
