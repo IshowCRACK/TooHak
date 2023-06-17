46808d7e3cc190d06e57ff015075bedd229909c0
IJLAL <z5310722@ad.unsw.edu.au>
2023-06-17 14:55:11 +1000

    fixed style

--- src/other.test.js
+++ src/other.test.js
@@ -5,25 +5,21 @@ import { getData } from './dataStore.js';
 import { adminAuthRegister } from './auth.js'
 
 test('Test successful clear', () => {
-    let person1_id = adminAuthRegister('good.l@gl.com','Password123','Joh nny-Bone',"Jo'nes");
-    //let quiz1_id= adminQuizCreate(person1_id,'how to drop comp1531', 'A comprehensive quiz to test students knowledge on how to quickly drop comp1531');
-    
-    clear();
-    expect(getData()).toStrictEqual(
-        {
-            users: [
-                {
+	let person1_id = adminAuthRegister('good.l@gl.com','Password123','Joh nny-Bone',"Jo'nes");
+	//let quiz1_id= adminQuizCreate(person1_id,'how to drop comp1531', 'A comprehensive quiz to test students knowledge on how to quickly drop comp1531');
+	
+	clear();
+	expect(getData()).toStrictEqual({
+		users: [
+					{
 
+				}
+			],
+		quizes: [
+					{
 
-                }
-            ],
-            quizes: [
-                {
-
-                    
-                }
-            ],
-        } 
-    );
-  });
+				}
+			],
+	});
+});
   
